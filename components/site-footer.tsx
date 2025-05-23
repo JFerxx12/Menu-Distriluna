95844470b0c2942fe67606a07345ca6395e01ed4
jhojax12866 <jhojanfer12@gmail.com>
2025-05-23 01:24:56 -0500

    code start derechos

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -5,23 +5,23 @@ export function SiteFooter() {
     <footer className="border-t border-blue-200 dark:border-blue-800 py-6 md:py-0 bg-blue-50 dark:bg-blue-950/20">
       <div className="container flex flex-col items-center justify-between gap-4 md:h-16 md:flex-row">
         <p className="text-center text-sm leading-loose text-blue-600 dark:text-blue-400 md:text-left">
-          © 2024 Distriluna. Todos los derechos reservados.
+          © 2025 CodeStart. Todos los derechos reservados.
         </p>
         <div className="flex gap-4">
           <Link
-            href="#"
+            href="https://code-start-git-main-jhojanfer12-gmailcoms-projects.vercel.app/"
             className="text-sm text-orange-500 dark:text-orange-400 underline underline-offset-4 hover:text-orange-600 dark:hover:text-orange-300"
           >
             Términos
           </Link>
           <Link
-            href="#"
+            href="https://code-start-git-main-jhojanfer12-gmailcoms-projects.vercel.app/"
             className="text-sm text-orange-500 dark:text-orange-400 underline underline-offset-4 hover:text-orange-600 dark:hover:text-orange-300"
           >
             Privacidad
           </Link>
           <Link
-            href="#"
+            href="https://code-start-git-main-jhojanfer12-gmailcoms-projects.vercel.app/"
             className="text-sm text-orange-500 dark:text-orange-400 underline underline-offset-4 hover:text-orange-600 dark:hover:text-orange-300"
           >
             Contacto
